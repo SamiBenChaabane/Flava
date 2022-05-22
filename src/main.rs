142fd21389cc81fa684379747a5d6743921ad2e2
Sami Ben Chaabane <sami.ben.chaabane10@gmail.com>
2022-05-22 21:03:41 +0100

    Stage files

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+mod generation;
+use ansi_term::Colour::Fixed;
+use generation::password_gen;
+fn main() {
+    let mut table_height: u8 = 0;
+    let mut table_width: u8 = 0;
+
+    print!("╭");
+    while table_width != 14 {
+        print!("─");
+        table_width += 1;
+    }
+
+    print!("╮\n│ {} │\n╰", Fixed(41).paint(password_gen()));
+
+    table_width = 0;
+
+    while table_width != 14 {
+        print!("─");
+        table_width += 1;
+    }
+    println!("╯");
+    //println!("╭─────────────────────────────────╮ \n│  │\n╰─────────────────────────────────╯");
+    //Fixed(41).paint(password_gen())
+}
