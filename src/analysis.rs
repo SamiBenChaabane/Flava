e41d5dba2c70e85503764b71090f9b013f9f2f97
Sami Ben Chaabane <sami.ben.chaabane10@gmail.com>
2022-06-10 09:05:24 +0100

    Overhauled Email regex

--- src/analysis.rs
+++ src/analysis.rs
@@ -3,12 +3,10 @@ use regex::Regex;
 use regex::RegexSet;
 
 pub struct PasswordReport {
-    //Sensitive info vectors
     pub email_captures: Vec<String>,
     pub dates_captures: Vec<String>,
     pub credit_card_numbers_captures: Vec<String>,
     pub entropy: f32,
-    //Weak complexity members
 }
 impl PasswordReport {
     pub fn password_analysis(&mut self, password: &str) {
@@ -21,22 +19,26 @@ impl PasswordReport {
                 none overlapping captures so if two emails were next to each other in the password a TLD could
                 eat up part of the next email or worse, it could eat it all up and make it undetectable.
                 */
-                r"[a-z0-9+[\.]{0,1}?+]{1,32}@([a-z0-9]+([\-]{1}[a-z0-9]+)*\.(com|net|tn|it|fr|jp|ar|co\.uk|com\.br|de|ru|br|co\.in|it|es|in|ca|ch|com\.au|co\.jp|nl|com\.ar|com\.mx|nl|co\.id|com\.sg|net\.au|io|org|com\.tn|com\.fr|int\.ar|net\.ar|org\.ar|au|net\.au|nz|net\.nz|com\.nz|com\.es|net\.es|co))",
+                r"(?i)[a-z0-9+[\.]{0,1}?+]{1,32}@([a-z0-9]+([\-]{1}[a-z0-9]+)*\.(com\.[a-z]{2,3}?|com|net\.[a-z]{2,3}?|net|gov\.[a-z]{2,3}?|gov|org\.[a-z]{2,3}?|org|tn|it|fr|jp|ar|au|de|ru|br|in|uk|ar|nl|nz|es|id|io|it|ca|ch))",
                 )
             .unwrap();
+
             static ref DATES: Regex = Regex::new(
                 r"[0-9]{4}[\.|\-|\\|/]{0,1}?+(((0[13578]|(10|12))[\.|\-|\\|/]{0,1}?+(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)[\.|\-|\\|/]{0,1}?+(0[1-9]|[1-2][0-9]|30)))",
                 )
             .unwrap();
+
             static ref CREDIT_CARD_NUMBERS: Regex = Regex::new(
                 r"(4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11})",
                 )
             .unwrap();
+
             static ref CHAR_MATCHER: RegexSet = RegexSet::new(&[
-            r"[[:lower:]]",
-            r"[[:upper:]]",
-            r"[[:digit:]]",
-            r"[[:punct:]]"]).unwrap();
+                r"[[:lower:]]",
+                r"[[:upper:]]",
+                r"[[:digit:]]",
+                r"[[:punct:]]"]).unwrap();
+
         }
         for capture in EMAILS.captures_iter(password) {
             self.email_captures.push(capture[0].to_string());
